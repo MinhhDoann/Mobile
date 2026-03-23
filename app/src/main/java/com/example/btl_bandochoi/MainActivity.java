d76b0c63f50a92a0817d935b2112f7f84e75ae54
duyhuy25 <duyhuy1342005@gmail.com>
2026-03-23 11:35:02 +0700

    update

--- app/src/main/java/com/example/btl_bandochoi/MainActivity.java
+++ app/src/main/java/com/example/btl_bandochoi/MainActivity.java
@@ -2,23 +2,28 @@ package com.example.btl_bandochoi;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.btl_bandochoi.adapter.ProductAdapter;
+import com.example.btl_bandochoi.data.FakeData;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.sanpham);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        adapter = new ProductAdapter(FakeData.getProducts());
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/btl_bandochoi/SanPham.java
@@ -0,0 +1,12 @@
+package com.example.btl_bandochoi;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sanpham extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sanpham);
+    }
+}
