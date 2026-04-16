aa96aa0c1f3852bbba9a062e12776427999a82ec
duyhuy25 <duyhuy1342005@gmail.com>
2026-04-16 23:17:25 +0700

    update search

--- app/src/main/java/com/example/btl_bandochoi/MainActivity.java
+++ app/src/main/java/com/example/btl_bandochoi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.btl_bandochoi;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
